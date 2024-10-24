b668eb1cb5e4eda186122bfe0071f71cc826beed
natashaseroff <natashaseroff@mail.ru>
2024-10-24 17:55:53 +0200

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fakeM
+import true_math as trueM
+
+result1 = fakeM.divide(69, 3)
+result2 = fakeM.divide(3, 0)
+result3 = trueM.divide(49, 7)
+result4 = trueM.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
